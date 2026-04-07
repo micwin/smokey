6c90fefdc8503edc0e1181985fa304a819175308
Michael Winkler <michael.winkler@gmx.de>
2026-04-07 17:23:56 +0200

    cleanup: remove self test

--- tests.d/010-selftests.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-TEST_ROOT="${SMOKEY_TEST_ROOT:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}"
-PROJECT_ROOT="$(cd "${TEST_ROOT}/.." && pwd)"
-
-"${PROJECT_ROOT}/smokey" --tests-dir "${PROJECT_ROOT}/selftests.d"
